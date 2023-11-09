f36b49a602695f670dad86e493ef6b97ef82780d
dotnetcsharp0x <dsotnikov1996@gmail.com>
2023-11-09 12:32:21 +0300

    Добавьте файлы проекта.

---
+++ Skymey-crypto-okex-current-prices/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Skymey_crypto_okex_current_prices
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
